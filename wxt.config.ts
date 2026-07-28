66cb59e53d0edc3098e81545f9ca87dcbfa13d59
Otaro <9078877+oovz@users.noreply.github.com>
2026-07-27 19:04:53 -0500

    chore: bump to 1.6.1

--- wxt.config.ts
+++ wxt.config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
 
     return {
       name: "__MSG_extName__",
-      version: "1.6.0",
+      version: "1.6.1",
       description: "__MSG_extDescription__",
       default_locale: "en",
       minimum_chrome_version: "150",
